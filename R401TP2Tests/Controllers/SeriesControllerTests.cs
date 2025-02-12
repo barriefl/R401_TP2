e4ceb9f3616407895d94c33da093e4476c0ea579
barriefl <Florian.Barrier@etu.univ-smb.fr>
2025-02-12 11:15:44 +0100

    Ajout du projet tests unitaires du Controller.

---
+++ R401TP2Tests/Controllers/SeriesControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using R401TP2.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace R401TP2.Controllers.Tests
+{
+    [TestClass()]
+    public class SeriesControllerTests
+    {
+        [TestMethod()]
+        public void GetSeriesTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
