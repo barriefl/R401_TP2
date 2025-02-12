7b595ca7aa9d0a466d69c7dcd398ad5e48d01a2a
barriefl <Florian.Barrier@etu.univ-smb.fr>
2025-02-12 10:26:17 +0100

    Scaffolding

---
+++ R401TP2/Models/EntityFramework/Serie.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace R401TP2.Models.EntityFramework;
+
+public partial class Serie
+{
+    public int Serieid { get; set; }
+
+    public string Titre { get; set; } = null!;
+
+    public string? Resume { get; set; }
+
+    public int? Nbsaisons { get; set; }
+
+    public int? Nbepisodes { get; set; }
+
+    public int? Anneecreation { get; set; }
+
+    public string? Network { get; set; }
+}
