551d81b7b8d321c29c4548e10198f16f16bee264
barriefl <Florian.Barrier@etu.univ-smb.fr>
2025-02-12 11:06:04 +0100

    Ajout d'un nouveau package NuGet.

--- R401TP2/Program.cs
+++ R401TP2/Program.cs
@@ -1,6 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using R401TP2.Models.EntityFramework;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<SeriesDbContext>(options =>
+  options.UseNpgsql(builder.Configuration.GetConnectionString("SeriesDbContext")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
